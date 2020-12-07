63c76bf80bfa43025f16ab0f67a50eda7968f12c
corycalaway <cory.c.calaway@gmail.com>
2020-12-06 20:55:05 -0700

    added hardcoded git search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,16 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+// get repo from github using api
+var getUserRepos = function(user) {
+    // formath the github api url using user as variable to select different profiles.
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data)
+        });
+    });
+};
+   
+  getUserRepos('corycalaway');
+
+  //6.2.2
