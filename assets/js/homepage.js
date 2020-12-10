cd33d4663e2750435ec38326b80a4de0c0c6d2cb
corycalaway <cory.c.calaway@gmail.com>
2020-12-10 10:29:09 -0700

    added searching by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -92,5 +93,31 @@ for (var i = 0; i < repos.length; i++) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      });
+    };
+
+var buttonClickHandler = function(event) {
+var language = event.target.getAttribute("data-language")
+if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+console.log(language)
+}
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
